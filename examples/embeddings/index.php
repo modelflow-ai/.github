3e3102d3dd4f53991865b0431d18a8594bee2a6f
Johannes Wachter <johannes@sulu.io>
2024-02-19 21:31:22 +0100

    add filesystem-embedding-store

--- examples/embeddings/index.php
+++ examples/embeddings/index.php
@@ -17,7 +17,7 @@ use ModelflowAi\Embeddings\Adapter\Cache\CacheEmbeddingAdapter;
 use ModelflowAi\Embeddings\Formatter\EmbeddingFormatter;
 use ModelflowAi\Embeddings\Generator\EmbeddingGenerator;
 use ModelflowAi\Embeddings\Splitter\EmbeddingSplitter;
-use ModelflowAi\Embeddings\Store\Memory\MemoryEmbeddingsStore;
+use ModelflowAi\Embeddings\Store\Memory\FilesystemEmbeddingsStore;
 use ModelflowAi\Ollama\Ollama;
 use ModelflowAi\OllamaAdapter\Embeddings\OllamaEmbeddingAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
@@ -34,7 +34,11 @@ $embeddingAdapter = new CacheEmbeddingAdapter(
     new FilesystemAdapter('ollama', 0, __DIR__ . '/var/cache'),
 );
 $embeddingGenerator = new EmbeddingGenerator($embeddingSplitter, $embeddingFormatter, $embeddingAdapter);
-$memoryStore = new MemoryEmbeddingsStore();
+
+if (\file_exists(__DIR__ . '/var/embeddings.txt')) {
+    \unlink(__DIR__ . '/var/embeddings.txt');
+}
+$store = new FilesystemEmbeddingsStore(__DIR__ . '/var/embeddings.txt');
 
 $input = [
     new ExampleEmbedding(\file_get_contents(__DIR__ . '/var/books/schildbuerger.txt') ?: '', 'schildbuerger.txt'),
@@ -42,10 +46,10 @@ $input = [
 ];
 $output = $embeddingGenerator->generateEmbeddings($input);
 
-$memoryStore->addDocuments($output);
+$store->addDocuments($output);
 
 $vector = $embeddingAdapter->embedText('Welches Tier hat die Wittwe?');
-$result = $memoryStore->similaritySearch($vector, 4, ['fileName' => 'schildbuerger.txt']);
+$result = $store->similaritySearch($vector, 4, ['fileName' => 'schildbuerger.txt']);
 
 foreach ($result as $item) {
     echo $item->getContent() . \PHP_EOL . \PHP_EOL;
---
+++ packages/embeddings/src/Store/Memory/FilesystemEmbeddingsStore.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Modelflow AI package.
+ *
+ * (c) Johannes Wachter <johannes@sulu.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ModelflowAi\Embeddings\Store\Memory;
+
+use ModelflowAi\Embeddings\Algorithm\DistanceL2Utils;
+use ModelflowAi\Embeddings\Model\EmbeddingInterface;
+use ModelflowAi\Embeddings\Store\EmbeddingsStoreInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+/**
+ * Heavenly inspired by LLPhant
+ * https://github.com/theodo-group/LLPhant/blob/main/src/Embeddings/VectorStores/FileSystem/FileSystemVectorStore.php.
+ */
+class FilesystemEmbeddingsStore implements EmbeddingsStoreInterface
+{
+    public function __construct(
+        public string $filePath,
+    ) {
+    }
+
+    public function addDocument(EmbeddingInterface $embedding): void
+    {
+        $embeddingsList = $this->readDocumentsFromFile();
+        $embeddingsList[] = $embedding;
+        $this->saveDocumentsToFile($embeddingsList);
+    }
+
+    public function addDocuments(array $embeddings): void
+    {
+        $embeddingsList = $this->readDocumentsFromFile();
+        $embeddingsList = \array_merge($embeddingsList, $embeddings);
+        $this->saveDocumentsToFile($embeddingsList);
+    }
+
+    public function similaritySearch(array $vector, int $k = 4, array $additionalArguments = []): array
+    {
+        $distances = [];
+        $embeddings = $this->readDocumentsFromFile();
+
+        $accessor = new PropertyAccessor();
+
+        foreach ($embeddings as $index => $embedding) {
+            foreach ($additionalArguments as $key => $value) {
+                if ($accessor->getValue($embedding, $key) !== $value) {
+                    continue 2;
+                }
+            }
+
+            if (null === $embedding->getVector()) {
+                continue;
+            }
+
+            $dist = DistanceL2Utils::euclideanDistanceL2($vector, $embedding->getVector());
+            $distances[$index] = $dist;
+        }
+
+        \asort($distances); // Sort by distance (ascending).
+
+        $topKIndices = \array_slice(\array_keys($distances), 0, $k, true);
+
+        $results = [];
+        foreach ($topKIndices as $index) {
+            $results[] = $embeddings[$index];
+        }
+
+        return $results;
+    }
+
+    /**
+     * @return EmbeddingInterface[]
+     */
+    private function readDocumentsFromFile(): array
+    {
+        if (!\file_exists($this->filePath)) {
+            return [];
+        }
+
+        /** @var EmbeddingInterface[] $result */
+        $result = \unserialize((string) \file_get_contents($this->filePath));
+
+        return $result;
+    }
+
+    /**
+     * @param EmbeddingInterface[] $embeddings
+     */
+    private function saveDocumentsToFile(array $embeddings): void
+    {
+        \file_put_contents($this->filePath, \serialize($embeddings));
+    }
+}
