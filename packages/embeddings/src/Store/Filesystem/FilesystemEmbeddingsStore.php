6a9ec1ee49c896dde8b133064b037e088d1aa557
Johannes Wachter <johannes@sulu.io>
2024-02-26 20:02:25 +0000

    fix namespace for filesystem store (#33)

--- packages/embeddings/src/Store/Filesystem/FilesystemEmbeddingsStore.php
+++ packages/embeddings/src/Store/Filesystem/FilesystemEmbeddingsStore.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace ModelflowAi\Embeddings\Store\Memory;
+namespace ModelflowAi\Embeddings\Store\Filesystem;
 
 use ModelflowAi\Embeddings\Algorithm\DistanceL2Utils;
 use ModelflowAi\Embeddings\Model\EmbeddingInterface;
